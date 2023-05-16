3b8905bd7aa5c51027a9a74d45435078d8a61ef6
yaric <yaric@yared>
2023-05-15 20:51:10 -0700

    this is my second project

---
+++ TestGitProject2/src/Tes2/test2.java
@@ -0,0 +1,11 @@
+package Tes2;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		System.out.print("this is my second git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestGitProject2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yaric
+ *
+ */
+module TestGitProject2 {
+}
